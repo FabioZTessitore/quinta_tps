6c078e5238b4f5f4c3757a2134f2f4fb3d54b47d
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-11-13 12:16:45 +0100

    thread multipli

---
+++ 06_socket/chat_thread_multiple/server.py
@@ -0,0 +1,44 @@
+# server.py
+
+import socket
+from threading import Thread
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind( ('', 9000) )
+
+s.listen(5)
+
+clients_list = []
+
+def listenToClient(c):
+    global clients_list
+
+    while True:
+        data = c.recv(1000)
+        data_str = str(data, 'utf-8')
+        print(data_str)
+        
+        if data_str == 'end':
+            print('sending end of communication')
+            c.send(bytes('end', 'utf-8'))
+            c.close()
+            clients_list.remove(c)
+            break
+        else:
+            for client in clients_list:
+                if client != c:
+                    client.send(data)
+
+            
+
+
+while True:
+    c, a = s.accept()
+    print('Connessione da', a)
+
+    clients_list.append(c)
+
+    t = Thread(target=listenToClient, args=(c,))
+    t.start()
