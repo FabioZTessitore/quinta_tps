854411b7be282b35a79f5405bfab2653a720a26b
Fabio Z Tessitore <fabioztessitore@gmail.com>
2020-03-04 21:18:17 +0100

    libri

--- 11_libri/index.js
+++ 11_libri/index.js
@@ -53,11 +53,49 @@ app.post('/edit-autore', function (req, res) {
 });
 
 app.get('/libri', function (req, res) {
-    const sql = "SELECT * FROM libri;";
-    db.all(sql, function (err, rows) {
-        res.render('libri', {
-            libri: rows
+    const sql_libri = "SELECT libri.id, libri.titolo, autori.cognome FROM libri LEFT JOIN autori ON libri.id_autore=autori.id ORDER BY titolo;";
+    const sql_autori = "SELECT * FROM autori ORDER BY cognome;";
+    db.all(sql_autori, function (err, autori) {
+        db.all(sql_libri, function (err, libri) {
+            res.render('libri', {
+                autori: autori,
+                libri: libri
+            });
         });
+    });
+});
+
+app.post('/nuovo-libro', function (req, res) {
+    const sql = "INSERT INTO libri (titolo, id_autore) VALUES (?, ?);";
+    db.run(sql, [req.body.titolo, req.body.autore], function () {
+        res.redirect('/libri');
+    });
+});
+
+app.get('/libro/:id/edit', function (req, res) {
+    const sql_libri = "SELECT * FROM libri WHERE id=?;";
+    const sql_autori = "SELECT * FROM autori ORDER BY cognome;";
+    db.all(sql_autori, function (err, autori) {
+        db.get(sql_libri, [req.params.id], function (err, libro) {
+            res.render('libro', {
+                autori: autori,
+                libro: libro
+            });
+        });
+    });
+});
+
+app.post('/edit-libro', function (req, res) {
+    const sql = "UPDATE libri SET titolo=?, id_autore=? WHERE id=?;";
+    db.run(sql, [req.body.titolo, req.body.autore, req.body.id], function () {
+        res.redirect('/libri');
+    });
+});
+
+app.post('/delete-libro', function (req, res) {
+    const sql = "DELETE FROM libri WHERE id=?;";
+    db.run(sql, [req.body.id], function () {
+        res.redirect('/libri');
     });
 });
 
